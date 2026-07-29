5c126d9de5701fe43b9323f0fdd8cff5b7408c57
narcisolobo <narcisolobo@gmail.com>
2026-07-29 09:54:17 -0700

    moved the funky sun, lifted navbar z-index

--- apps/rebuild/src/sections/hero/funky-sun/FunkySun.tsx
+++ apps/rebuild/src/sections/hero/funky-sun/FunkySun.tsx
@@ -4,19 +4,21 @@ import adapter from "@/images/adapter.svg";
 
 function FunkySun() {
   return (
-    <div className="relative flex min-h-130 items-center justify-center">
-      <div id="funky-sun" className={styles["funky-sun"]}></div>
-      <div id="vinyl" className={styles.vinyl}></div>
-      <div
-        id="adapter"
-        className={`motion-safe:animate-spin ${styles.adapter}`}
-      >
-        <Image
-          src={adapter}
-          loading="eager"
-          alt="An image of a 45 RPM record adapter"
-          className="block h-full w-full object-cover"
-        />
+    <div className="absolute -right-20 -bottom-20">
+      <div className="relative flex min-h-130 items-center justify-center">
+        <div id="funky-sun" className={styles["funky-sun"]}></div>
+        <div id="vinyl" className={styles.vinyl}></div>
+        <div
+          id="adapter"
+          className={`motion-safe:animate-spin ${styles.adapter}`}
+        >
+          <Image
+            src={adapter}
+            loading="eager"
+            alt="An image of a 45 RPM record adapter"
+            className="block h-full w-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
